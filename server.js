758bec060523e3e6d37241987a8ea91c33d324d9
James Crutchley <ne0rmatrix@gmail.com>
2022-11-15 14:04:00 -0800

    major updates

---
+++ server.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let app = express();
+
+let Parser = require('rss-parser');
+let parser = new Parser();
+let showTitle;
+let podcast = [];
+test = async () => {
+    let feed = await parser.parseURL('https://feeds.twit.tv/sn_video_hd.xml');
+    console.log(feed.title + '\n');
+    showTitle = feed.title;
+    feed.items.forEach(item => {
+        const show = {title: item.title, link: item.guid ,details: item.content}
+        podcast.push(show);
+      //  console.log('\n' + item.title + ':' + item.content + '\n' + item.guid)
+})
+}
+test();
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        showTitle,podcast
+    })
+})
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
